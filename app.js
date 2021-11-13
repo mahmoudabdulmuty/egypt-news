836748647d49378f5ede4832cdc3f2f36a5a137d
Mahmoud <mahmoudabdulmuty@gmail.com>
2021-11-13 03:08:06 +0200

    adding categories

--- app.js
+++ app.js
@@ -1,18 +1,17 @@
 const apiLink = 'https://newsapi.org/v2/top-headlines?country=eg';
 const apiKey = '&apiKey=3bd4753c68144c04b3eb73e44b7da657';
+const articleEl = document.createElement('article');
 
-document.addEventListener('DOMContentLoaded', function () {
+function createArticle(category) {
+	articleEl.textContent = '';
 	const xhr = new XMLHttpRequest();
-	xhr.open('GET', apiLink + apiKey);
-
+	xhr.open('GET', apiLink + category + apiKey);
 	xhr.onload = function () {
 		const articles = JSON.parse(this.response).articles;
-		const articleEl = document.createElement('article');
-		articleEl.textContent = '';
 		if (this.status === 200) {
 			articles.forEach((article) => {
 				const { title, description, url, urlToImage } = article;
-				articleEl.innerHTML += `				  
+				articleEl.innerHTML += `
         <article class="article">
         <div class="article-img">
           <img src="${urlToImage || ''}"/>
@@ -27,6 +26,37 @@ document.addEventListener('DOMContentLoaded', function () {
 		}
 		document.querySelector('.main').append(articleEl);
 	};
-
 	xhr.send();
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+	createArticle('');
+});
+
+document.querySelector('.header-title').addEventListener('click', function () {
+	createArticle('');
+});
+
+document.querySelector('.business').addEventListener('click', function () {
+	createArticle('&category=business');
+});
+
+document.querySelector('.entertainment').addEventListener('click', function () {
+	createArticle('&category=entertainment');
+});
+
+document.querySelector('.health').addEventListener('click', function () {
+	createArticle('&category=health');
+});
+
+document.querySelector('.science').addEventListener('click', function () {
+	createArticle('&category=science');
+});
+
+document.querySelector('.sports').addEventListener('click', function () {
+	createArticle('&category=sports');
+});
+
+document.querySelector('.technology').addEventListener('click', function () {
+	createArticle('&category=technology');
 });
