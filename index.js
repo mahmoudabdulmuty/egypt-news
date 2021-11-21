0957b488abd70255dc61a159e187f03bbca34e30
Mahmoud <mahmoudabdulmuty@gmail.com>
2021-11-21 20:39:05 +0200

    updating apikey

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function createArticle(
 	category = '',
 	apiLink = 'https://newsapi.org/v2/top-headlines?country=eg'
 ) {
-	const apiKey = '&apiKey=a94d31166e044e38b8a83507ca9942a3';
+	const apiKey = '&apiKey=3bd4753c68144c04b3eb73e44b7da657';
 	document.querySelector('.main').textContent = '';
 
 	const xhr = new XMLHttpRequest();
@@ -90,68 +90,3 @@ document
 		)
 			searchQuery();
 	});
-
-// function createArticle(
-// 	category = '',
-// 	apiLink = 'https://newsapi.org/v2/top-headlines?country=eg'
-// ) {
-// 	const apiKey = '&apiKey=a94d31166e044e38b8a83507ca9942a3';
-// 	document.querySelector('.main').textContent = '';
-
-// 	fetch(apiLink + category + apiKey)
-// 		.then((response) => {
-// 			return response.json();
-// 		})
-// 		.then((responseText) => {
-// 			const articles = responseText.articles;
-// 			if (articles.length === 0)
-// 				document.querySelector('.main').innerHTML = 'No data Found';
-// 			else {
-// 				articles.forEach((article) => {
-// 					const { title, description, url, urlToImage } = article;
-// 					document.querySelector('.main').innerHTML += `
-// 	      <article class="article">
-// 	      <div class="article-img">
-// 	        <img src="${urlToImage ?? ''}"/>
-// 	      </div>
-// 	      <div class="article-heading">
-// 	        <h2 class="article-title">${title}</h2>
-// 	        <p class="article-description">${description ?? ''}</p>
-// 	        <a class="article-link" target="_blank" href="${url}">قراءة المزيد</a>
-// 	      </div>
-// 	    </article>`;
-// 				});
-// 			}
-// 		});
-// }
-
-// async function createArticle(
-// 	category = '',
-// 	apiLink = 'https://newsapi.org/v2/top-headlines?country=eg'
-// ) {
-// 	const apiKey = '&apiKey=a94d31166e044e38b8a83507ca9942a3';
-// 	document.querySelector('.main').textContent = '';
-
-// 	const response = await fetch(apiLink + category + apiKey);
-// 	const responseText = await response.json();
-// 	console.log(responseText.articles);
-// 	const articles = responseText.articles;
-// 	if (articles.length === 0)
-// 		document.querySelector('.main').innerHTML = 'No data Found';
-// 	else {
-// 		articles.forEach((article) => {
-// 			const { title, description, url, urlToImage } = article;
-// 			document.querySelector('.main').innerHTML += `
-// 	      <article class="article">
-// 	      <div class="article-img">
-// 	        <img src="${urlToImage ?? ''}"/>
-// 	      </div>
-// 	      <div class="article-heading">
-// 	        <h2 class="article-title">${title}</h2>
-// 	        <p class="article-description">${description ?? ''}</p>
-// 	        <a class="article-link" target="_blank" href="${url}">قراءة المزيد</a>
-// 	      </div>
-// 	    </article>`;
-// 		});
-// 	}
-// }
